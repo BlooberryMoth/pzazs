3e03a835784ae91b3dfa070b1d43015675333478
BlooberryMoth <namoyedlaz@gmail.com>
2025-08-24 16:43:44 -0600

    Updated the REST API for new formatting on my machine

--- APIServer.py
+++ APIServer.py
@@ -34,44 +34,43 @@ class Server(BaseHTTPRequestHandler):
         self.wfile.write(data)
 
     
+    # Returns are formated as headers, status_code, and data. Errors are handled as unpacked (status_code, data)
     def handle_GET_request_response(self, url: list[str], parameters: dict) -> list[list[tuple[str, str]], int, dict]:
-        # Returns are formated as headers, status_code, and data. Errors are handled as unpacked (status_code, data)
-        if url[0] == "api":
-            headers = [("Content-Type", "application/json")]
-
+        headers = [("Content-Type", "application/json")]
+        if url[0] == "":
             # Returns the data from the Client that's updated in /threads/update_api.py
-            if len(url) == 1: return headers, 200, {"userCount": user_count, "serverCount": server_count}
+            return headers, 200, {"userCount": user_count, "serverCount": server_count}
 
-            # Returns the HTTP user's shared guilds with the Client
-            elif url[1] == "getguilds":
-                try: token = parameters['token']
-                except: return headers, *error(401, "No API key/access token provided.")
-                try: guilds = fetch("/guilds", token)
-                except: return headers, *error(401, "Invalid API key/access token.")
+        # Returns the HTTP user's shared guilds with the Client
+        if url[0] == "getguilds":
+            try: token = parameters['token']
+            except: return headers, *error(401, "No API key/access token provided.")
+            try: guilds = fetch("/guilds", token)
+            except: return headers, *error(401, "Invalid API key/access token.")
 
-                client_guild_IDs = [guild.id for guild in Client.guilds]
-                return headers, 200, [guild for guild in guilds if int(guild['id']) in client_guild_IDs]
+            client_guild_IDs = [guild.id for guild in Client.guilds]
+            return headers, 200, [guild for guild in guilds if int(guild['id']) in client_guild_IDs]
 
-            # Returns files from /http/res
-            elif url[1] == "res":
-                url = "/".join(url); assert isinstance(url, str)
+        # Returns files from /http/res
+        elif url[0] == "res":
+            url = "/".join(url); assert isinstance(url, str)
+            try:
+                with open(f"./http/{url}", "rb") as data: return [], 200, data.read()
+            except: return [("Content-Type", "application/json")], *error(404, f"File \"./{url}\" not found.")
+
+        elif url[0] == "login":
+            if len(url) == 1: return [("Location", login_redirect)], 307, bytes("", 'utf-8')
+
+            if url[1] == "callback":
                 try:
-                    with open(f"./http{url.removeprefix('api')}", "rb") as data: return [], 200, data.read()
-                except: return [("Content-Type", "application/json")], *error(404, f"File \"./{url}\" not found.")
-
-            elif url[1] == "login":
-                if len(url) == 2: return [("Location", login_redirect)], 307, bytes("", 'utf-8')
-
-                if url[2] == "callback":
-                    try:
-                        data = OAuth2.get_tokens(parameters['code'])
-                        headers = [("Set-Cookie", f"discord_access_token={data['access_token']}; Domain=pzazs.thatgalblu.com; Path=/")]
-                    except: return [("Content-Type", "application/json")], *error(400, "Unable to get Discord user access token.")
-                    else:
-                        headers.append(("Location", "https://pzazs.thatgalblu.com"))
-                    
-                        return headers, 308, bytes("", 'utf-8')
-                    
+                    data = OAuth2.get_tokens(parameters['code'])
+                    headers = [("Set-Cookie", f"discord_access_token={data['access_token']}; Domain=pzazs.thatgalblu.com; Path=/")]
+                except: return [("Content-Type", "application/json")], *error(400, "Unable to get Discord user access token.")
+                else:
+                    headers.append(("Location", "https://pzazs.thatgalblu.com"))
+                
+                    return headers, 308, bytes("", 'utf-8')
+                
         return [], 404, bytes("", 'utf-8')
 
     # Disable the HTTP logs
